7dc3779b0fad6d022f4b97c78b506546089620eb
24LP2674 24LP2674 <24LP2674@setups-iMac-2.local>
2025-05-08 12:30:48 +0800

    be setup using prisma

---
+++ backend/src/controller/user.ts
@@ -0,0 +1,71 @@
+import { Request, Response } from "express";
+import { prisma } from "../../utils/prisma";
+import { log } from "console";
+import bcrypt from "bcrypt";
+
+export const createUser = async (req: Request, res: Response) => {
+  const { email, name, password } = req.body;
+  const hashedPass = await bcrypt.hash(password, 5);
+  try {
+    const Response = await prisma.user.create({
+      data: {
+        name,
+        password: hashedPass,
+        email,
+      },
+    });
+    console.log(res);
+    return res.send({
+      data: Response,
+    });
+  } catch (error) {
+    return res.send(error);
+  }
+};
+
+export const getUser = async (res: Response) => {
+  try {
+    const response = await prisma.user.findMany();
+    return res.send({
+      success: true,
+      data: response,
+    });
+  } catch (error) {
+    return res.send(error);
+  }
+};
+
+export const updataUserById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  console.log(id, "id");
+
+  const { email, name, password } = req.body;
+  try {
+    const response = await prisma.user.update({
+      where: { id: Number(id) },
+      data: { email, password, name },
+    });
+    return res.send({
+      success: true,
+      data: response,
+    });
+  } catch (error) {
+    return res.send(error);
+  }
+};
+
+export const deleteUserById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const response = await prisma.user.delete({
+      where: { id: Number(id) },
+    });
+    return res.send({
+      success: true,
+      message: "user deleted",
+    });
+  } catch (error) {
+    return res.send(error);
+  }
+};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,11 @@
-import express from "express";
-
+import express, { json } from "express";
+import cors from "cors";
+import { userRouter } from "./routes/user";
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
+app.use(json());
+app.use(cors());
+app.use("/user", userRouter);
 
 app.listen(8080, () => {
   console.log(`server running at http://localhost:8080`);
