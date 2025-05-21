a8ad1b72903d6524d0147216df8d81454fb2a0df
24LP2674 24LP2674 <24LP2674@setups-iMac-2.local>
2025-05-21 19:00:33 +0800

    home page

--- frontend/src/app/view-page/page.tsx
+++ frontend/src/app/view-page/page.tsx
@@ -108,7 +108,7 @@ export default function page() {
                     render={({ field }) => (
                       <FormItem className="items-start flex flex-col">
                         <FormLabel>
-                          Enter BuyMeCoffee or social acount URL:
+                          Enter a BuyMeCoffee or social acount URL:
                         </FormLabel>
                         <FormControl>
                           <Input
